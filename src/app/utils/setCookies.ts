d5eae168547272727e601a90229e6b4fcd0431ea
MoRsHeD <mdmorshed0187@gmail.com>
2025-09-13 22:28:27 +0600

    changed secure and logics in logout function

--- src/app/utils/setCookies.ts
+++ src/app/utils/setCookies.ts
@@ -9,13 +9,15 @@ export const setCookies = (res: Response, tokenInfo: AuthTokens) => {
     if (tokenInfo.accessToken) {
         res.cookie('accessToken', tokenInfo.accessToken, {
             httpOnly: true,
-            secure: true
+            secure: true,
+            sameSite : 'none'
         });
     };
     if (tokenInfo.refreshToken) {
         res.cookie('refreshToken', tokenInfo.refreshToken, {
             httpOnly: true,
-            secure: true
+            secure: true,
+            sameSite : 'none'
         });
     }
 }
